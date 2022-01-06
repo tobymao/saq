0cb240912aa3b6deeec7d363ccf70087f5703a0e
tobymao <toby.mao@gmail.com>
2022-01-05 23:05:19 -0800

    fix example

--- examples/simple.py
+++ examples/simple.py
@@ -6,7 +6,7 @@ from saq import Queue
 
 
 async def sleeper(ctx, *, a):
-    await asyncio.sleep(1)
+    await asyncio.sleep(a)
     return {"a": a}
 
 
@@ -17,17 +17,17 @@ async def adder(ctx, *, a, b):
 
 settings = {
     "functions": [sleeper, adder],
-    "concurrency": 10,
+    "concurrency": 100,
 }
 
 async def enqueue(func, **kwargs):
     queue = Queue.from_url("redis://localhost")
     for _ in range(10000):
-        await queue.enqueue(func, **kwargs)
+        await queue.enqueue(func, **{k: v() for k, v in kwargs.items()})
 
 
 if __name__ == "__main__":
     now = time.time()
-    asyncio.run(enqueue("sleeper", a=random.randint(0, 100)))
-    asyncio.run(enqueue("adder", a=random.randint(0, 100), b=random.randint(0, 100)))
+    asyncio.run(enqueue("sleeper", a=random.random))
+    asyncio.run(enqueue("adder", a=random.random, b=random.random))
     print(time.time() - now)
