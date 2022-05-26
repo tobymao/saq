09392562e95854867e7f575c623f6ec0904d85d8
aprilahijriyan <hijriyan23@gmail.com>
2022-05-26 16:58:53 +0700

    🌈 style(black): run format.sh

--- saq/worker.py
+++ saq/worker.py
@@ -281,7 +281,7 @@ def import_settings(settings):
 
 
 def start(settings, web=False, extra_web_settings=None, port=8080):
-    sys.path.insert(0, os.getcwd()) # fix: https://github.com/tobymao/saq/issues/26
+    sys.path.insert(0, os.getcwd())  # fix: https://github.com/tobymao/saq/issues/26
     settings = import_settings(settings)
 
     if "queue" not in settings:
