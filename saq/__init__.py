728bd2c38b5e0e5f47ce7be93ec22516982eaba3
Peter Schutt <peter.github@proton.me>
2022-10-30 14:30:02 +1000

    Adds `__all__` for explict reexport of top level names.

--- saq/__init__.py
+++ saq/__init__.py
@@ -2,4 +2,12 @@ from saq.job import CronJob, Job, Status
 from saq.queue import Queue
 from saq.worker import Worker
 
+__all__ = [
+    "CronJob",
+    "Job",
+    "Queue",
+    "Status",
+    "Worker",
+]
+
 __version__ = "0.9.0"
