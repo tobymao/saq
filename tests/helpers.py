ca7960d2194f63e29c243918ed92ab6276a28877
Vincent Chan <vpchan84@gmail.com>
2024-08-28 12:23:34 -0700

    Use test_saq schema for tests

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,5 +1,7 @@
 import typing as t
 
+import psycopg
+
 from saq.queue import Queue
 from saq.queue.postgres import PostgresQueue
 from saq.queue.redis import RedisQueue
@@ -12,21 +14,21 @@ async def create_redis_queue(**kwargs: t.Any) -> RedisQueue:
 
 
 async def create_postgres_queue(**kwargs: t.Any) -> PostgresQueue:
+    with psycopg.connect("postgres://postgres@localhost", autocommit=True) as conn:
+        conn.execute("CREATE SCHEMA IF NOT EXISTS test_saq")
+
     queue = t.cast(
         PostgresQueue,
-        Queue.from_url("postgres://postgres@localhost", **kwargs),
+        Queue.from_url("postgres://postgres@localhost?options=--search_path%3Dtest_saq", **kwargs),
     )
     await queue.connect()
     return queue
 
 
 async def cleanup_queue(queue: Queue) -> None:
+    await queue.disconnect()
     if isinstance(queue, RedisQueue):
         await queue.redis.flushdb()
     elif isinstance(queue, PostgresQueue):
-        async with queue.pool.connection() as conn:
-            await conn.execute("DROP SCHEMA public CASCADE")
-            await conn.execute("CREATE SCHEMA public")
-            await conn.execute("GRANT ALL ON SCHEMA public TO postgres")
-            await conn.execute("GRANT ALL ON SCHEMA public TO public")
-    await queue.disconnect()
+        with psycopg.connect("postgres://postgres@localhost", autocommit=True) as conn:
+            conn.execute("DROP SCHEMA test_saq CASCADE")
